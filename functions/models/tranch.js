009949c891dd6c35394fa14511f092b1348e38b6
dwarade <dwarade@microsoft.com>
2024-02-09 18:57:22 +0530

    modified the validation for Tranche

--- functions/models/tranch.js
+++ functions/models/tranch.js
@@ -2,7 +2,7 @@ const Joi = require("joi");
 
 const Tranch = Joi.object({
 	name: Joi.string().required(),
-	percentage: Joi.number().required().positive().greater(0).max(100),
+	percentage: Joi.number().required().min(0).max(100),
 	isCommonTranch: Joi.bool().required(),
 });
 
