93afac3786c09737c861ef22edb1c396914b9379
swadhinbubaipaul <swadhinpaul159@gmail.com>
2024-05-21 18:14:01 +0530

    add logs in green score, green data and monitoring data

--- functions/services/greenScore.js
+++ functions/services/greenScore.js
@@ -1,20 +1,27 @@
+const { logger } = require("firebase-functions/v1");
 const { axiosHttpService } = require("./axioscall");
 
 const getGreenScore = async (email, projectId) => {
-	const scoreOptions = {
-		url: `${process.env.GREENSCORE_API_URI}/getScoreData`,
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-			"X-API-KEY": process.env.GREENSCORE_API_KEY,
-		},
-		data: {
-			email: email,
-			projectId: projectId,
-		},
-	};
-	const scoreResult = await axiosHttpService(scoreOptions);
-	return scoreResult;
+	try {
+		logger.info("greenScore getGreenScore execution started");
+		const scoreOptions = {
+			url: `${process.env.GREENSCORE_API_URI}/getScoreData`,
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				"X-API-KEY": process.env.GREENSCORE_API_KEY,
+			},
+			data: {
+				email: email,
+				projectId: projectId,
+			},
+		};
+		const scoreResult = await axiosHttpService(scoreOptions);
+		logger.info("greenScore getGreenScore execution end");
+		return scoreResult;
+	} catch (error) {
+		logger.error(error);
+	}
 };
 
 module.exports = {
